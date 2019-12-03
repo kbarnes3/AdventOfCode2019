b691bc8faea0928342611041d2f8f5b449a22726
Kevin Barnes <kbarnes3@gmail.com>
2019-12-03 11:31:18 -0800

    test data works

--- Day3_CrossedWires_Common/Data.h
+++ Day3_CrossedWires_Common/Data.h
@@ -1,1 +1,35 @@
 #pragma once
+#include <array>
+
+enum class Direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+};
+
+struct Movement
+{
+    Direction direction;
+    int distance;
+};
+
+// Transform with:
+// '<,'>s/D/    { Direction::Down, /g | '<,'>s/R/    { Direction::Right, /g | '<,'>s/U/    { Direction::Up, /g | '<,'>s/L/    { Direction::Left, /g | '<,'>s/\(\d\),/\1 },\r/g
+
+// R8,U5,L5,D3
+std::array<Movement, 4> test_path_1 = { {
+    { Direction::Right, 8 },
+    { Direction::Up, 5 },
+    { Direction::Left, 5 },
+    { Direction::Down, 3 },
+} };
+
+// U7,R6,D4,L4
+std::array<Movement, 4> test_path_2 = { {
+    { Direction::Up, 7 },
+    { Direction::Right, 6 },
+    { Direction::Down, 4 },
+    { Direction::Left, 4 },
+} };
--- Day3_CrossedWires_Part1/Day3_CrossedWires_Part1.cpp
+++ Day3_CrossedWires_Part1/Day3_CrossedWires_Part1.cpp
@@ -2,11 +2,85 @@
 //
 
 #include <Data.h>
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <limits>
+#include <map>
+#include <utility>
+#include <vector>
+#include <intrin.h>
+
+typedef std::function<void (int, int)> TracingCallback;
+
+template<size_t Size>
+void trace_path(const std::array<Movement, Size>& path, TracingCallback callback)
+{
+    int x = 0;
+    int y = 0;
+
+    for(const Movement& move : path)
+    {
+        for (int i = 0; i < move.distance; i++)
+        {
+            switch (move.direction)
+            {
+            case Direction::Up:
+                y++;
+                break;
+            case Direction::Down:
+                y--;
+                break;
+            case Direction::Left:
+                x--;
+                break;
+            case Direction::Right:
+                x++;
+                break;
+            default:
+                __fastfail(-1);
+            }
+
+            callback(x, y);
+        }
+    }
+}
+
+template<size_t Size1, size_t Size2>
+void solve(const std::array<Movement, Size1>& path1, const std::array<Movement, Size2>& path2)
+{
+    std::map<std::pair<int, int>, bool> path1Trace;
+
+    trace_path(path1, [&path1Trace](int x, int y) {
+        path1Trace.insert_or_assign(std::make_pair(x, y), true);
+    });
+
+    std::vector<std::pair<int, int>> intersections;
+
+    trace_path(path2, [&path1Trace, &intersections](int x, int y) {
+        std::pair<int, int> point = std::make_pair(x, y);
+        std::map<std::pair<int, int>, bool>::const_iterator iter = 
+            path1Trace.find(point);
+        if (iter != path1Trace.cend())
+        {
+            intersections.push_back(point);
+        }
+    });
+
+    int closestIntersection = std::numeric_limits<int>::max();
+    for (const std::pair<int, int>& point : intersections)
+    {
+        int distance = abs(point.first) + abs(point.second);
+        closestIntersection = std::min(closestIntersection, distance);
+    }
+
+    std::wcout << closestIntersection << L'\n';
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    solve(test_path_1, test_path_2);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
