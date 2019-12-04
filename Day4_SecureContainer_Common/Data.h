d612222e43e9cd5a92c4b6a44696887c78f8bbdf
Kevin Barnes <kbarnes3@gmail.com>
2019-12-04 10:29:48 -0800

    Test data works

--- Day4_SecureContainer_Common/Data.h
+++ Day4_SecureContainer_Common/Data.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include <utility>
+
+constexpr std::pair<int, int> test_data_1 = std::make_pair(111111, 111111);
+constexpr std::pair<int, int> test_data_2 = std::make_pair(223450, 223450);
+constexpr std::pair<int, int> test_data_3 = std::make_pair(123789, 123789);
+constexpr std::pair<int, int> real_data = std::make_pair(254032, 789860);
--- Day4_SecureContainer_Part1/Day4_SecureContainer_Part1.cpp
+++ Day4_SecureContainer_Part1/Day4_SecureContainer_Part1.cpp
@@ -2,11 +2,73 @@
 //
 
 #include <Data.h>
+#include <array>
 #include <iostream>
+
+std::array<int, 6> Decompose(int sixDigitNumber)
+{
+    int remainingDigits = sixDigitNumber;
+    std::array<int, 6> decomposed;
+
+    for (size_t i = 6; i > 0; i--)
+    {
+        int digit = remainingDigits % 10;
+        decomposed[i - 1] = digit;
+        remainingDigits = remainingDigits / 10;
+    }
+
+    return decomposed;
+}
+
+bool HasAdjacentMatch(const std::array<int, 6>& digits)
+{
+    for (size_t i = 0; i < 5; i++)
+    {
+        if (digits[i] == digits[i + 1])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool IsNeverDecreasing(const std::array<int, 6>& digits)
+{
+    for (size_t i = 1; i < 6; i++)
+    {
+        if (digits[i] < digits[i - 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Solve(const std::pair<int, int>& range)
+{
+    unsigned int matches = 0;
+    for (int test = range.first; test <= range.second; test++)
+    {
+        std::array<int, 6> digits = Decompose(test);
+        bool hasAdjacentMatch = HasAdjacentMatch(digits);
+        if (hasAdjacentMatch)
+        {
+            bool isNeverDecreasing = IsNeverDecreasing(digits);
+            if (isNeverDecreasing)
+            {
+                matches++;
+            }
+        }
+    }
+
+    std::wcout << matches << L'\n';
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solve(test_data_1);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
