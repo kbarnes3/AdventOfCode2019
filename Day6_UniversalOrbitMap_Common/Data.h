97da8bad2a1ebd65fbd838fe985a63d482feff96
Kevin Barnes <kbarnes3@gmail.com>
2019-12-06 11:37:37 -0800

    BuildOrbitMap returns

--- Day6_UniversalOrbitMap_Common/Data.h
+++ Day6_UniversalOrbitMap_Common/Data.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <string>
+#include <vector>
+
+struct OrbitEntry
+{
+    std::wstring Orbitee;
+    std::wstring Orbiter;
+};
+
+// Transform with
+// '<,'>s/\(\w*\))\(\w*\)/    { L"\1", L"\2" },
+
+std::vector<OrbitEntry> test_data = { {
+    { L"COM", L"B" },
+    { L"B", L"C" },
+    { L"C", L"D" },
+    { L"D", L"E" },
+    { L"E", L"F" },
+    { L"B", L"G" },
+    { L"G", L"H" },
+    { L"D", L"I" },
+    { L"E", L"J" },
+    { L"J", L"K" },
+    { L"K", L"L" },
+} };
+
--- Day6_UniversalOrbitMap_Part1/Day6_UniversalOrbitMap_Part1.cpp
+++ Day6_UniversalOrbitMap_Part1/Day6_UniversalOrbitMap_Part1.cpp
@@ -3,10 +3,69 @@
 
 #include <Data.h>
 #include <iostream>
+#include <map>
+#include <intrin.h>
+
+__declspec(noreturn)
+void FAIL_FAST()
+{
+    __fastfail(1);
+}
+
+struct OrbitNode
+{
+    std::wstring Name;
+    OrbitNode* Parent;
+};
+
+std::map<std::wstring, OrbitNode> BuildOrbitMap(std::vector<OrbitEntry>& entries)
+{
+    std::map<std::wstring, OrbitNode> orbitMap;
+    orbitMap.insert(
+        { L"COM", 
+        { L"COM", nullptr } });
+
+    while (!entries.empty())
+    {
+        std::vector<OrbitEntry>::iterator iter = entries.begin();
+        while (iter != entries.end())
+        {
+            std::map<std::wstring, OrbitNode>::iterator orbiteeNode =
+                orbitMap.find(iter->Orbitee);
+            if (orbiteeNode != orbitMap.end())
+            {
+                // We found the parent node in the map, add an entry pointing to it
+                std::pair<std::map<std::wstring, OrbitNode>::iterator, bool> result = orbitMap.insert(
+                    { iter->Orbiter,
+                    { iter->Orbiter, &(orbiteeNode->second) } });
+                if (!result.second)
+                {
+                    FAIL_FAST();
+                }
+                
+                // Now remove the current entry from the list we need to process still
+                iter = entries.erase(iter);
+            }
+            else
+            {
+                // The orbitee node hasn't been mapped yet. Just move on and we'll deal
+                // with this in another pass.
+                ++iter;
+            }
+        }
+    }
+
+    return orbitMap;
+}
+
+void Solve(std::vector<OrbitEntry>& entries)
+{
+    std::map<std::wstring, OrbitNode> orbitMap = BuildOrbitMap(entries);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solve(test_data);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
