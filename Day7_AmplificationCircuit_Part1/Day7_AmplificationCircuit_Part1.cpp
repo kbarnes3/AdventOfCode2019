a4c1ca5989fddc71c66294262aa9f6113a3ae899
Kevin Barnes <kbarnes3@gmail.com>
2019-12-10 11:14:20 -0800

    Test data works

--- Day7_AmplificationCircuit_Part1/Day7_AmplificationCircuit_Part1.cpp
+++ Day7_AmplificationCircuit_Part1/Day7_AmplificationCircuit_Part1.cpp
@@ -2,18 +2,45 @@
 //
 
 #include <Data.h>
+#include <algorithm>
 #include "Computer.h"
 #include <iostream>
+#include <limits>
+
+template<size_t Size>
+int MaximizePhaseSettings(std::array<int, Size>& intCode, const std::vector<int>& phaseSettingList, int inputSignal)
+{
+    if (phaseSettingList.size() == 0)
+    {
+        return inputSignal;
+    }
+
+    int maxThruster = std::numeric_limits<int>::min();
+
+    for (int phaseSetting : phaseSettingList)
+    {
+        std::array<int, 2> input = { phaseSetting, inputSignal };
+        Computer<Size, 2, true> computer(intCode, input);
+        int outputValue = computer.Process();
+
+        std::vector<int> innerPhaseSettingList = phaseSettingList;
+        std::vector<int>::iterator toErase = std::remove(innerPhaseSettingList.begin(), innerPhaseSettingList.end(), phaseSetting);
+        innerPhaseSettingList.erase(toErase, innerPhaseSettingList.end());
+
+        int thrusterValue = MaximizePhaseSettings(intCode, innerPhaseSettingList, outputValue);
+        maxThruster = std::max(maxThruster, thrusterValue);
+    }
+
+    return maxThruster;
+}
 
 template<size_t Size>
 void Solve(std::array<int, Size>& intCode)
 {
-    std::array<int, 2> input = { 5, 5 };
-    Computer<Size, 2, true> computer(intCode, input);
+    std::vector<int> phaseSettingList = { 0, 1, 2, 3, 4 };
+    int maxThruster = MaximizePhaseSettings(intCode, phaseSettingList, 0);
     
-    int value = computer.Process();
-    
-    std::wcout << value << L'\n';
+    std::wcout << maxThruster << L'\n';
 }
 
 int main()
