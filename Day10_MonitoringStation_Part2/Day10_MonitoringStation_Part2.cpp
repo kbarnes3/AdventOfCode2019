4e0f12a7fa9e9c1bf5b2603913a2ef9d2631945a
Kevin Barnes <kbarnes3@gmail.com>
2019-12-13 13:34:17 -0800

    Solving Day 10 Part 2

--- Day10_MonitoringStation_Part2/Day10_MonitoringStation_Part2.cpp
+++ Day10_MonitoringStation_Part2/Day10_MonitoringStation_Part2.cpp
@@ -2,8 +2,50 @@
 //
 
 #include <Data.h>
+#include <cmath>
+#include <intrin.h>
 #include <iostream>
+#include <map>
 #include <numeric>
+#include <optional>
+#include <utility>
+
+constexpr double pi = 3.1415926535897;
+
+__declspec(noreturn)
+void FAIL_FAST()
+{
+    __fastfail(1);
+}
+
+enum class State
+{
+    Empty,
+    Asteroid,
+    Occluded
+};
+
+template<size_t Width, size_t Height>
+void ConvertArray(
+    const std::array<std::array<bool, Width>, Height>& source,
+    std::array<std::array<State, Width>, Height>& dest
+)
+{
+    for (size_t y = 0; y < Height; y++)
+    {
+        for (size_t x = 0; x < Width; x++)
+        {
+            if (source[y][x])
+            {
+                dest[y][x] = State::Asteroid;
+            }
+            else
+            {
+                dest[y][x] = State::Empty;
+            }
+        }
+    }
+}
 
 template<size_t Width, size_t Height>
 bool IsAsteroidVisible(
@@ -66,9 +108,11 @@ unsigned int CountVisibleAsteroids(
 }
 
 template<size_t Width, size_t Height>
-void Solve(const std::array<std::array<bool, Width>, Height>& data)
+std::pair<size_t, size_t> GetStation(const std::array<std::array<bool, Width>, Height>& data)
 {
     unsigned int maxVisible = 0;
+    size_t stationX = 0;
+    size_t stationY = 0;
 
     for (size_t y = 0; y < Height; y++)
     {
@@ -77,18 +121,199 @@ void Solve(const std::array<std::array<bool, Width>, Height>& data)
             if (data[y][x])
             {
                 unsigned int visible = CountVisibleAsteroids(data, x, y);
-                maxVisible = std::max(maxVisible, visible);
+                if (visible > maxVisible)
+                {
+                    maxVisible = visible;
+                    stationX = x;
+                    stationY = y;
+                }
             }
         }
     }
 
-    std::wcout << maxVisible << L'\n';
+    return { stationX, stationY };
+}
 
+template<size_t Width, size_t Height>
+void BuildAngleMap(
+    std::array<std::array<State, Width>, Height>& states,
+    std::map<double, std::pair<size_t, size_t>>& angles,
+    size_t stationX,
+    size_t stationY)
+{
+    for (size_t y = 0; y < Height; y++)
+    {
+        for (size_t x = 0; x < Width; x++)
+        {
+            if (states[y][x] != State::Empty)
+            {
+                states[y][x] = State::Asteroid;
+                ptrdiff_t deltaX = x - stationX;
+                ptrdiff_t deltaY = stationY - y; // Swap this because position deltaY should be "up"
+
+                double angle = atan2(deltaX, deltaY);
+                if (angle < 0)
+                {
+                    angle += 2 * pi;
+                }
+                angles.insert_or_assign(angle, std::make_pair(x, y));
+            }
+        }
+    }
+}
+
+bool NextStepInBounds(size_t x, size_t y, ptrdiff_t xStep, ptrdiff_t yStep, size_t width, size_t height)
+{
+    if (xStep < 0)
+    {
+        if (static_cast<ptrdiff_t>(x) < abs(xStep))
+        {
+            return false;
+        }
+    }
+    else if (x + xStep >= width)
+    {
+        return false;
+    }
+
+    if (yStep < 0)
+    {
+        if (static_cast<ptrdiff_t>(y) < abs(yStep))
+        {
+            return false;
+        }
+    }
+    else if (y + yStep >= height)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+template<size_t Width, size_t Height>
+std::optional<std::pair<size_t, size_t>> DestroyAsteroidInLine(
+    std::array<std::array<State, Width>, Height>& states,
+    size_t stationX,
+    size_t stationY,
+    size_t lineX,
+    size_t lineY
+)
+{
+    ptrdiff_t xDiff = lineX - stationX;
+    ptrdiff_t yDiff = lineY - stationY;
+
+    // Destroy the closest asteroid in this line, mark
+    // all the others as obscured
+    ptrdiff_t gcd = std::gcd(xDiff, yDiff);
+
+    ptrdiff_t xStep = xDiff / gcd;
+    ptrdiff_t yStep = yDiff / gcd;
+
+    size_t x = stationX;
+    size_t y = stationY;
+
+    std::optional<std::pair<size_t, size_t>> destroyed = {};
+
+    do
+    {
+        x += xStep;
+        y += yStep;
+
+        if (states[y][x] != State::Empty)
+        {
+            if (!destroyed.has_value() && states[y][x] == State::Occluded)
+            {
+                // The closest asteroid is occluded
+                // This probably means we had two entires in our angle map
+                // That were slightly different floats representing the same angle
+                // Just return now
+                return {};
+            }
+
+            if (!destroyed)
+            {
+                states[y][x] = State::Empty;
+                destroyed = std::make_pair(x, y);
+            }
+            else
+            {
+                states[y][x] = State::Occluded;
+            }
+            
+        }
+    }
+    while (NextStepInBounds(x, y, xStep, yStep, Width, Height));
+
+    if (!destroyed.has_value())
+    {
+        // We shouldn't get here without finding one thing in this line to destroy
+        FAIL_FAST();
+    }
+    return destroyed;
+}
+
+template<size_t Width, size_t Height>
+std::pair<size_t, size_t> DestroyAsteroids(
+    std::array<std::array<State, Width>, Height>& states,
+    size_t stationX,
+    size_t stationY,
+    size_t targetCount)
+{
+    // Make sure we remove the station from the array of asteroids to destroy
+    states[stationY][stationX] = State::Empty;
+    size_t destroyedCount = 0;
+
+    while (destroyedCount < targetCount)
+    {
+        std::map<double, std::pair<size_t, size_t>> angles;
+        BuildAngleMap(states, angles, stationX, stationY);
+
+        if (angles.empty())
+        {
+            return std::make_pair(0, 0);
+        }
+
+        for (const std::pair<double, std::pair<size_t, size_t>>& value : angles)
+        {
+            std::optional<std::pair<size_t, size_t>> destroyed = DestroyAsteroidInLine(
+                states,
+                stationX, 
+                stationY,
+                value.second.first,
+                value.second.second);
+            
+            if (destroyed.has_value())
+            {
+                destroyedCount++;
+            }
+
+            if (destroyedCount == targetCount)
+            {
+                return destroyed.value();
+            }
+        }
+    }
+
+    return std::make_pair(0, 0);
+}
+
+template<size_t Width, size_t Height>
+void Solve(const std::array<std::array<bool, Width>, Height>& data, size_t targetCount)
+{
+    std::pair<size_t, size_t> station = GetStation(data);
+    std::array<std::array<State, Width>, Height> states;
+    ConvertArray(data, states);
+
+    std::pair<size_t, size_t> destroyed = DestroyAsteroids(states, station.first, station.second, targetCount);
+
+    size_t value = 100 * destroyed.first + destroyed.second;
+    std::wcout << value << L'\n';
 }
 
 int main()
 {
-    Solve(real_data);
+    Solve(real_data, 200);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
