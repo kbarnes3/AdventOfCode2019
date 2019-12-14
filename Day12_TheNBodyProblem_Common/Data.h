79027f39b71712114b8c8fd0845a3f2ac6452492
Kevin Barnes <kbarnes3@gmail.com>
2019-12-14 11:54:48 -0800

    Test data works

--- Day12_TheNBodyProblem_Common/Data.h
+++ Day12_TheNBodyProblem_Common/Data.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <array>
+
+struct Position
+{
+    int x;
+    int y;
+    int z;
+};
+
+// Transform with:
+// '<,'>s/</    { /g | '<,'>s/\w=//g | '<,'>s/>/ },/g
+const std::array<Position, 4> test_data = { {
+    { -1, 0, 2 },
+    { 2, -10, -7 },
+    { 4, -8, 8 },
+    { 3, 5, -1 },
+} };
--- Day12_TheNBodyProblem_Part1/Day12_TheNBodyProblem_Part1.cpp
+++ Day12_TheNBodyProblem_Part1/Day12_TheNBodyProblem_Part1.cpp
@@ -1,11 +1,138 @@
 // Day12_TheNBodyProblem_Part1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <Data.h>
 #include <iostream>
+#include <numeric>
+
+struct Velocity
+{
+    int x;
+    int y;
+    int z;
+};
+
+struct Moon
+{
+    Position position;
+    Velocity velocity;
+};
+
+template<size_t Size>
+void ApplyGravity(std::array<Moon, Size>& moons)
+{
+    typename std::array<Moon, Size>::iterator moon = moons.begin();
+    while (moon != moons.end())
+    {
+        typename std::array<Moon, Size>::const_iterator otherMoon = moons.cbegin();
+        while (otherMoon != moons.cend())
+        {
+            if (moon->position.x < otherMoon->position.x)
+            {
+                moon->velocity.x++;
+            }
+            else if (moon->position.x > otherMoon->position.x)
+            {
+                moon->velocity.x--;
+            }
+
+            if (moon->position.y < otherMoon->position.y)
+            {
+                moon->velocity.y++;
+            }
+
+            else if (moon->position.y > otherMoon->position.y)
+            {
+                moon->velocity.y--;
+            }
+
+            if (moon->position.z < otherMoon->position.z)
+            {
+                moon->velocity.z++;
+            }
+            else if (moon->position.z > otherMoon->position.z)
+            {
+                moon->velocity.z--;
+            }
+
+            ++otherMoon;
+        }
+
+        moon++;
+    }
+}
+
+template<size_t Size>
+void ApplyVelocity(std::array<Moon, Size>& moons)
+{
+    typename std::array<Moon, Size>::iterator moon = moons.begin();
+    while (moon != moons.end())
+    {
+        moon->position.x += moon->velocity.x;
+        moon->position.y += moon->velocity.y;
+        moon->position.z += moon->velocity.z;
+        ++moon;
+    }
+}
+
+template<size_t Size>
+void PrintMoons(const std::array<Moon, Size>& moons)
+{
+    for (const Moon& moon : moons)
+    {
+        std::wcout << L"pos=<x=" << moon.position.x << L", y=" << moon.position.y;
+        std::wcout << L", z=" << moon.position.z << L">, vel=<x=" << moon.velocity.x;
+        std::wcout << L", y=" << moon.velocity.y << L", z=" << moon.velocity.z;
+        std::wcout << L">\n";
+    }
+}
+
+template<size_t Size>
+int ComputeEnergy(const std::array<Moon, Size>& moons)
+{
+    return std::accumulate(
+        moons.cbegin(),
+        moons.cend(),
+        0,
+        [](int a, const Moon& b) -> int
+        {
+            int value = a;
+            int potentialEnergy = abs(b.position.x) +
+                abs(b.position.y) +
+                abs(b.position.z);
+            int kineticEnergy = abs(b.velocity.x) +
+                abs(b.velocity.y) +
+                abs(b.velocity.z);
+            
+            value += potentialEnergy * kineticEnergy;
+            return value;
+        });
+}
+
+template<size_t Size>
+void Solve(const std::array<Position, Size>& data, unsigned int timeSteps)
+{
+    std::array<Moon, Size> moons;
+
+    for (size_t i = 0; i < Size; i++)
+    {
+        moons[i] = { { data[i].x, data[i].y, data[i].z }, { 0, 0, 0 } };
+    }
+
+    for (unsigned int t = 0; t < timeSteps; t++)
+    {
+        ApplyGravity(moons);
+        ApplyVelocity(moons);
+    }
+
+    int totalEnergy = ComputeEnergy(moons);
+    std::wcout << totalEnergy << L'\n';
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solve(test_data, 10);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
