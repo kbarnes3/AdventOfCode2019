4d75fe31e4207c9c46e6015fda32ac3c2370ad19
Kevin Barnes <kbarnes3@gmail.com>
2019-12-18 11:30:19 -0800

    Test data works

--- Day14_SpaceStoichiometry_Common/Data.h
+++ Day14_SpaceStoichiometry_Common/Data.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <map>
+#include <string>
+#include <vector>
+
+const std::wstring Ore = L"ORE";
+
+struct ReactionComponent
+{
+    int Quantity;
+    std::wstring Name;
+};
+
+struct Reaction
+{
+    std::vector<ReactionComponent> Inputs;
+    ReactionComponent Output;
+};
+
+// Transform with:
+// '<,'>s/\(.*\) \(\w\+\)$/    { L"\2", { { \1 \2 } },/ | '<,'>s/\(\d\+\) \(\w\+\)/{ \1, L"\2" }/g | '<,'>s/ =>/ },
+const std::map<std::wstring, Reaction> test_data_1 = { {
+    { L"A", { { { 10, L"ORE" } }, { 10, L"A" } } },
+    { L"B", { { { 1, L"ORE" } }, { 1, L"B" } } },
+    { L"C", { { { 7, L"A" }, { 1, L"B" } }, { 1, L"C" } } },
+    { L"D", { { { 7, L"A" }, { 1, L"C" } }, { 1, L"D" } } },
+    { L"E", { { { 7, L"A" }, { 1, L"D" } }, { 1, L"E" } } },
+    { L"FUEL", { { { 7, L"A" }, { 1, L"E" } }, { 1, L"FUEL" } } },
+} };
--- Day14_SpaceStoichiometry_Part1/Day14_SpaceStoichiometry_Part1.cpp
+++ Day14_SpaceStoichiometry_Part1/Day14_SpaceStoichiometry_Part1.cpp
@@ -2,11 +2,63 @@
 //
 
 #include <Data.h>
+#include <FailFast.h>
 #include <iostream>
+
+void Solve(const std::map<std::wstring, Reaction>& data)
+{
+    std::map<std::wstring, int> chemicals = { { { L"FUEL", -1 } } };
+    bool done = false;
+    do
+    {
+        done = true;
+        for (const std::pair<std::wstring, int>& chemical : chemicals)
+        {
+            // Skip ore procesing
+            if (chemical.first == Ore)
+            {
+                continue;
+            }
+            // Don't worry about chemicals where we have all we need currently
+            if (chemical.second >= 0)
+            {
+                continue;
+            }
+
+            // We found something we need a reaction to make
+            // Consider us not done for this loop
+            done = false;
+            std::map<std::wstring, Reaction>::const_iterator reactionIter = data.find(chemical.first);
+            if (reactionIter == data.cend())
+            {
+                FAIL_FAST();
+            }
+            const Reaction& reaction = reactionIter->second;
+
+            if (reaction.Output.Name != chemical.first)
+            {
+                FAIL_FAST();
+            }
+
+            // Add the output to our stockpiles, subtract the inputs from out stockpiles
+            chemicals[chemical.first] += reaction.Output.Quantity;
+            for (const ReactionComponent& component : reaction.Inputs)
+            {
+                chemicals[component.Name] -= component.Quantity;
+            }
+        }
+    }
+    while (!done);
+
+    int oreNeeded = chemicals[Ore];
+    oreNeeded *= -1;
+
+    std::wcout << oreNeeded << L'\n';
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solve(test_data_1);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
