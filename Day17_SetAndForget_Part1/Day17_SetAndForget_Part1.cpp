bc917e97b3a560932386cc4194ebbbb1d7af00b6
Kevin Barnes <kbarnes3@gmail.com>
2019-12-20 10:23:46 -0800

    Solving Day 17 Part 1

--- Day17_SetAndForget_Part1/Day17_SetAndForget_Part1.cpp
+++ Day17_SetAndForget_Part1/Day17_SetAndForget_Part1.cpp
@@ -4,10 +4,83 @@
 #include <Computer.h>
 #include <Data.h>
 #include <iostream>
+#include <vector>
+
+void PrintCameraOutput(const std::vector<std::vector<char>>& output)
+{
+    for (const std::vector<char>& row : output)
+    {
+        for (char character : row)
+        {
+            std::cout << character;
+        }
+        std::cout /*<< "\\n"*/ << '\n';
+    }
+}
+
+template<size_t Size>
+void Solve(const std::array<long long, Size>& intCode)
+{
+    Computer<long long, true> computer(intCode.cbegin(), intCode.cend());
+    std::vector<std::vector<char>> cameraOutput;
+
+    std::vector<std::vector<char>>::iterator currentRow = 
+        cameraOutput.insert(cameraOutput.cend(), std::vector<char>());
+    
+    std::optional<long long> output;
+    do
+    {
+        output = computer.Process();
+        if (!output.has_value())
+        {
+            break;
+        }
+
+        if (output.value() == 10)
+        {
+            // End of line, make a new line
+            currentRow = cameraOutput.insert(cameraOutput.cend(), std::vector<char>());
+        }
+        else
+        {
+            currentRow->push_back(static_cast<char>(output.value()));
+        }
+        
+    } while (output.has_value());
+
+    // PrintCameraOutput(cameraOutput);
+    
+    // Intersections can't occur on the outside edges
+    size_t alignmentSum = 0;
+    for (size_t y = 1; y < cameraOutput.size() - 1; y++)
+    {
+        if (cameraOutput[y].size() < 2)
+        {
+            continue;
+        }
+        for (size_t x = 1; x < cameraOutput[y].size() - 1; x++)
+        {
+            if (cameraOutput[y - 1].size() > x &&
+                cameraOutput[y + 1].size())
+            {
+                if (cameraOutput[y][x] != '.' &&
+                    cameraOutput[y][x - 1] != '.' &&
+                    cameraOutput[y][x + 1] != '.' &&
+                    cameraOutput[y - 1][x] != '.' &&
+                    cameraOutput[y + 1][x] != '.')
+                {
+                    alignmentSum += x * y;
+                }
+            }
+        }
+    }
+
+    std::wcout << alignmentSum << L'\n';
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solve(real_data);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
